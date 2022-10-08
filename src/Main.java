dd0803616cd3d8c568268e2a246bfa60585f1005
Lena Niku <niku.elena@gmail.com>
2022-10-09 01:10:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        HashMap<Character, Integer> symbol = new HashMap<>();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (Character.isLetter(c)) {
+                if (symbol.containsKey(c)) {
+                    symbol.put(c, symbol.get(c) + 1);
+                } else {
+                    symbol.put(c, 1);
+                }
+            }
+        }
+        int max = -1;
+        char maxC = 0;
+        for (char cM : symbol.keySet()) {
+            if (symbol.get(cM) > max) {
+                max = symbol.get(cM);
+                maxC = cM;
+            }
+        }
+        System.out.println("Максимальная частота символа " + maxC + " " + max);
+    }
+
+}
+
+
